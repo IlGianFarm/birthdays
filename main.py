95e916bb61978604032ab0ebe8643abaf812e8db
alemusca <ale98musca@gmail.com>
2019-11-25 19:45:17 +0100

    change main.py 2

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 #! /usr/bin/env python3
 
-from birthdays import return_birthday
-
-return_birthday('Albert Einstein')
-return_birthday('Alan Turing')
-
 import argparse
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("name", 
+    parser.add_argument("name", required = True,
                          help = "whose birthday would you like to know",
                          choices=)
-    parser.add_argument("date"
+    parser.add_argument("date", 
+                        help = "provide a date to check if it is anyone's 
+                        birthday")
+    args = parser.parse_args()
+    return args
+    
+if __name__ == "__main__":
+    args = parse_arguments()
+    print('user has requested "{}" birthday'.format(args.name, args.date))

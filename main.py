3e5ed7e296db8955e0377fd57d26392bdb402ad7
alemusca <ale98musca@gmail.com>
2019-11-25 19:18:12 +0100

    change main.py

--- main.py
+++ main.py
@@ -4,3 +4,11 @@ from birthdays import return_birthday
 
 return_birthday('Albert Einstein')
 return_birthday('Alan Turing')
+
+import argparse
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("name", 
+                         help = "whose birthday would you like to know",
+                         choices=)
+    parser.add_argument("date"

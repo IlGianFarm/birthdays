b876c5e9e4f6139921024aa6ecf36a32cb189444
Ubuntu <ubuntu@ip-172-31-32-48.eu-central-1.compute.internal>
2019-11-28 17:38:11 +0000

    update files

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 #! /usr/bin/env python3
 
 import argparse
+import csv_reader.py
+
 def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("name", required = True,
                          help = "whose birthday would you like to know",
-                         choices=)
+                         choices=names)
     parser.add_argument("date", 
-                        help = "provide a date to check if it is anyone's 
-                        birthday")
+                        help = "provide a date to check if it is anyone\'s  birthday")
     args = parser.parse_args()
     return args
     
 if __name__ == "__main__":
     args = parse_arguments()
-    print('user has requested "{}" birthday'.format(args.name, args.date))
+    print('user has requested "{}"\'s birthday'.format(args.name, args.date))
+

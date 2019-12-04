ebbf50e35adeaca3240e25ee797bd2638462a060
IlGianFarm <gianluca.schiavon@student.h-farm.com>
2019-12-04 13:26:55 +0100

    add and update files

--- Python_Package/csv_reader.py
+++ Python_Package/csv_reader.py
@@ -1,13 +1,19 @@
 import csv
 
-def csv_reader():
-    names=[]
-    dates=[]
-    with open("Birthdays.csv") as csv_file:
-        csv_reader = csv.reader(csv_file , delimiter=";")
-        for row in csv_reader:
-	        names.append(row[0])
-            dates.append(row[1])
+dataset={}
 
-if __name__==__"main"__:
-    csv_reader()
+def dictcreater():
+    with open("Birthdays.csv") as csv_file:
+        csv_reader=csv.reader(csv_file, delimiter=",")
+        for row in csv_reader:
+            dataset[row[0]]=row[1]
+dictcreater()
+
+names=dataset.keys()
+dates=dataset.values()
+
+if __name__=="__main__":
+    dictcreater()
+    print(dataset)
+    print(names)
+    print(dates)

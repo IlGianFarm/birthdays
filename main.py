4d36fb6de46b1d8909fd798c1577f20633de8c78
IlGianFarm <gianluca.schiavon@student.h-farm.com>
2019-12-15 09:11:19 +0100

    update sign_up, log_in and main

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 #! /usr/bin/env python3
 
-from Python_Package import csv_reader
-from Python_Package import argname
 import sys
+from scripts import log_in
+from Python_Package import argsmain
+from Python_Package import csv_reader
+from Python_Package import birthdays
 
-csv_reader.dictcreater()
+argsmain.argsmain()
+argsmain = argsmain.argsmain()
 
-argname.argnames()
+log_in.open_and_create()
 
-argname.attempt1()
-argname.attempt2()
+username = '{}'.format(arguments.credentials[0][0])
+password = '{}'.format(arguments.credentials[0][1])
+
+if log_in.check_user(username, password):
+    csv_reader_test.dictcreater()
+
+    name1 = "{}".format(arguments.names[0][0])
+    name2 = "{}".format(arguments.names[0][1])
+
+    birthdays.attempt1(name1)
+    birthdays.attempt2(name2)

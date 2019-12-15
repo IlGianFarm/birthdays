b479c8fdcaa29404b053c8ff57e6c3585908c7db
IlGianFarm <gianluca.schiavon@student.h-farm.com>
2019-12-15 08:29:16 +0100

    update reader

--- Python_Package/csv_reader.py
+++ Python_Package/csv_reader.py
@@ -1,19 +1,23 @@
 import csv
 
-dataset={}
+dataset = {}
+
 
 def dictcreater():
     with open("Birthdays.csv") as csv_file:
-        csv_reader=csv.reader(csv_file, delimiter=",")
+        csv_reader = csv.reader(csv_file, delimiter=",")
         for row in csv_reader:
-            dataset[row[0]]=row[1]
-dictcreater()
+            dataset[row[0]] = row[1]
 
-names=dataset.keys()
-dates=dataset.values()
 
-if __name__=="__main__":
+names = dataset.keys()
+dates = dataset.values()
+
+
+if __name__ == "__main__":
     dictcreater()
+    names = dataset.keys()
+    dates = dataset.values()
     print(dataset)
     print(names)
     print(dates)
